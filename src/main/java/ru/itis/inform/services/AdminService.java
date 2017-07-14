65e99c60a2b0aaaf67093ec383e1d651b16236fc
KarimovKamil <imanymuch@gmail.com>
2017-07-14 12:56:01 +0300

    add service interfaces

---
+++ src/main/java/ru/itis/inform/services/AdminService.java
@@ -0,0 +1,34 @@
+package ru.itis.inform.services;
+
+import ru.itis.inform.models.Request;
+import ru.itis.inform.models.User;
+
+import java.util.List;
+
+/**
+ * Created by Kamil Karimov on 14.07.2017.
+ */
+public interface AdminService {
+
+    String login(String login, String password);
+
+    User profile(String token);
+
+    List<Request> getAllRequests(String token);
+
+    List<Request> getActiveRequests(String token);
+
+    List<Request> getLabRequests(String token);
+
+    List<Request> getPracticeRequests(String token);
+
+    List<Request> getElectiveRequests(String token);
+
+    List<Request> getLeaderRequests(String token);
+
+    Request getRequestById(String token, long id);
+
+    void acceptRequest(String token, long id, String resMessage);
+
+    void declineRequest(String token, long id, String resMessage);
+}
--- src/main/java/ru/itis/inform/services/StudentService.java
+++ src/main/java/ru/itis/inform/services/StudentService.java
@@ -1,7 +1,25 @@
 package ru.itis.inform.services;
 
+import ru.itis.inform.dto.ProfileDto;
+import ru.itis.inform.models.Request;
+
+import java.util.List;
+
 /**
  * Created by Kamil Karimov on 06.07.2017.
  */
 public interface StudentService {
+
+    String login(String login, String password);
+
+    ProfileDto getProfile(String token);
+
+    List<Request> getMyRequests(String token);
+
+    Request getRequestById(String token, long id);
+
+    void addRequest(String token, RequestDto requestDto);
+
+    void deleteRequest(String token, long id);
+
 }
