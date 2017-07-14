3a2190999b9a9cab3c41da52fb9d8b99477604ff
artur <antegzeperob@gmail.com>
2017-07-14 13:07:45 +0300

    delete imports

--- src/main/java/ru/itis/inform/configs/WebAppContext.java
+++ src/main/java/ru/itis/inform/configs/WebAppContext.java
@@ -1,14 +1,9 @@
 package ru.itis.inform.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by artur on 14.07.2017.
